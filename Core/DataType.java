76e4882f7d7a766044801012fb4cda18c5e63f39
pawankumar-arya <pawankumararya.in@gmail.com>
2026-08-18 16:28:26 +0530

    first commit

---
+++ Core/DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+  public static void main(String[] args) {
+    int integerVar = 42;
+    double doubleVar = 3.14;
+    char charVar = 'A';
+    boolean booleanVar = true;
+    float floatVar = 2.5f;
+    System.out.println("Integer: " + integerVar);
+    System.out.println("Double: " + doubleVar);
+    System.out.println("Character: " + charVar);
+    System.out.println("Boolean: " + booleanVar);
+    System.out.println("Float: " + floatVar);
+    
+  }
+}
---
+++ Core/UserInput.java
@@ -0,0 +1,9 @@
+public class UserInput {
+  public static void main(String[] args) {
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
+    scanner.close();
+  }
+}
