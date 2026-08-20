545f9c69c734c64a622c24ecb70e4ac0fb523fb3
pawankumar-arya <pawankumararya.in@gmail.com>
2026-08-20 13:25:49 +0530

    patterns question

--- Patterns/LeftRightAngle.java
+++
@@ -1,5 +0,0 @@
-public class LeftRightAngle {
-  public static void main(String[] args){
-    int num=5 ;
-  }
-}
--- Patterns/SquareBox.java
+++ Patterns/SquareBox.java
@@ -1,8 +1,7 @@
-public class SquareBox{
+public class SquareBox {
   public static void main(String[]args){
-    int num=5 ;
-    for(int i=0; i<num; i++){
-      for(int j=0; j<num; j++){
+    for(int i=0; i<5; i++){
+      for(int j=0; j<5; j++){
         System.out.print("* ");
       }
       System.out.println();
