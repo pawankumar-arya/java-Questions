399098ebf0bb6ec40580c771ab67d19912fff692
pawankumar-arya <pawankumararya.in@gmail.com>
2026-08-20 13:04:11 +0530

    patterns

---
+++ Patterns/LeftRightAngle.java
@@ -0,0 +1,5 @@
+public class LeftRightAngle {
+  public static void main(String[] args){
+    int num=5 ;
+  }
+}
---
+++ Patterns/SquareBox.java
@@ -0,0 +1,11 @@
+public class SquareBox{
+  public static void main(String[]args){
+    int num=5 ;
+    for(int i=0; i<num; i++){
+      for(int j=0; j<num; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
